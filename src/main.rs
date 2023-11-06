554e5ed8ade7148f99936578a8ee124a3c0da4c8
helfezer <quent2.p92@gmail.com>
2023-11-06 14:18:41 +0000

    Add spi0 initialisation

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,16 @@ use panic_probe as _;
 use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
+// Embed the `Hz` function/trait:
+use fugit::RateExtU32;
+
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
+    gpio,
+    spi,
 };
 
 #[entry]
@@ -58,7 +63,21 @@ fn main() -> ! {
     // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
     // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
     // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut led_pin = pins.gpio16.into_push_pull_output();
+    
+    let spi_sclk: gpio::Pin<_, gpio::FunctionSpi, gpio::PullNone> = pins.gpio2.reconfigure();
+    let spi_mosi: gpio::Pin<_, gpio::FunctionSpi, gpio::PullNone> = pins.gpio3.reconfigure();
+    let spi_miso: gpio::Pin<_, gpio::FunctionSpi, gpio::PullUp> = pins.gpio4.reconfigure();
+    let spi_cs = pins.gpio5.into_push_pull_output();
+
+    let spi = spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
+
+    let spi = spi.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        400.kHz(),
+        embedded_hal::spi::MODE_0,
+    );
 
     loop {
         info!("on!");
